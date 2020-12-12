5b7bf66579abd77ee060131617b0dd311061ff17
David Sherret <dsherret@gmail.com>
2020-12-11 19:25:25 -0500

    Re-export some useful parts of swc.

--- rs-lib/src/lib.rs
+++ rs-lib/src/lib.rs
@@ -7,3 +7,8 @@ mod types;
 pub use comments::CommentsIterator;
 pub use generated::*;
 pub use types::*;
+
+// swc re-exports
+pub use swc_common::comments::{Comment, CommentKind};
+pub use swc_common::{BytePos, Span, Spanned};
+pub use swc_ecmascript::parser::token::{Token, TokenAndSpan};
