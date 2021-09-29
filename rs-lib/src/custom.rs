03d40e6351a256dbc16a84d881f7eeb4e28a4ecd
David Sherret <dsherret@gmail.com>
2021-09-29 15:53:43 -0400

    feat: add `.ctxt()` to `Ident` and `BindingIdent`

--- rs-lib/src/custom.rs
+++ rs-lib/src/custom.rs
@@ -12,12 +12,20 @@ pub type Id = (JsWord, SyntaxContext);
 
 impl<'a> Ident<'a> {
   pub fn to_id(&self) -> Id {
-    (self.inner.sym.clone(), self.inner.span.ctxt())
+    (self.sym().clone(), self.ctxt())
+  }
+
+  pub fn ctxt(&self) -> SyntaxContext {
+    self.inner.span.ctxt
   }
 }
 
 impl<'a> BindingIdent<'a> {
   pub fn to_id(&self) -> Id {
     self.id.to_id()
+  }
+
+  pub fn ctxt(&self) -> SyntaxContext {
+    self.id.ctxt()
   }
 }
