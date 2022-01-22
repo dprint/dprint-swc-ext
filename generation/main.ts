b01072d3dba529adda38b723a190ef701a991add
David Sherret <dsherret@gmail.com>
2022-01-21 20:18:05 -0500

    refactor: remove serialize feature as it's not used

--- generation/main.ts
+++ generation/main.ts
@@ -1,11 +1,6 @@
 import { analyze } from "./analyze/analyze.ts";
 import { generate } from "./generate/generate.ts";
-import { generateSerialize } from "./generate/generate_serialize.ts";
-import { generateTypeScriptSetup, generateTypeScriptTypes } from "./generate/generate_ts.ts";
 
 const analysisResult = analyze();
 
 Deno.writeTextFileSync("./rs-lib/src/generated.rs", generate(analysisResult));
-Deno.writeTextFileSync("./rs-lib/src/serialize/serialize_generated.rs", generateSerialize(analysisResult));
-Deno.writeTextFileSync("./deno/types.generated.ts", generateTypeScriptTypes(analysisResult));
-Deno.writeTextFileSync("./deno/setup.generated.ts", generateTypeScriptSetup(analysisResult));
--- rs-lib/src/lib.rs
+++ rs-lib/src/lib.rs
@@ -8,17 +8,11 @@ mod types;
 #[rustfmt::skip]
 mod generated;
 
-#[cfg(feature = "serialize")]
-mod serialize;
-
 pub use comments::CommentsIterator;
 pub use custom::*;
 pub use generated::*;
 pub use source_file::*;
 pub use types::*;
-
-#[cfg(feature = "serialize")]
-pub use serialize::*;
 
 #[cfg(test)]
 mod test_helpers;
